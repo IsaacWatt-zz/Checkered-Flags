86733b1f01106532d452f58dadb45e32188469dd
Isaac Watt <isaacwatt@icloud.com>
2017-08-16 21:31:17 -0400

    final push 1/2

---
+++ js/checkeredflags.js
@@ -0,0 +1,23 @@
+/**
+ * Checkered Flags JS
+ * Controls anchor tag scrolling on Glossary page
+ */
+
+// The function actually applying the offset
+function offsetAnchor() {
+    if (location.hash.length !== 0) {
+        window.scrollTo(window.scrollX, window.scrollY - 145);
+    }
+}
+
+// Captures click events of all <a> elements with href starting with #
+$(document).on('click', 'a[href^="#"]', function (event) {
+    // Click events are captured before hashchanges. Timeout
+    // causes offsetAnchor to be called after the page jump.
+    window.setTimeout(function () {
+        offsetAnchor();
+    }, 0);
+});
+
+// Set the offset when entering page with hash present in the url
+window.setTimeout(offsetAnchor, 0);
